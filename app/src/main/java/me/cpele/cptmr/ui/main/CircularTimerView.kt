5c375d6e4f01658886edd1aee34c03540f198416
cpele <geekarist@users.noreply.github.com>
2021-11-24 18:46:31 +0100

    Draw current time

--- app/src/main/java/me/cpele/cptmr/ui/main/CircularTimerView.kt
+++ app/src/main/java/me/cpele/cptmr/ui/main/CircularTimerView.kt
@@ -22,24 +22,60 @@ class CircularTimerView @JvmOverloads constructor(
 
     private val halfStrokeWidth = paintStrokeWidth / 2
 
-    private val paint = Paint().apply {
+    private val circlePaint = Paint().apply {
         color = resources.getColor(android.R.color.holo_red_dark, context.theme)
         strokeWidth = paintStrokeWidth
         style = Paint.Style.STROKE
     }
 
+    private val arcPaint = Paint().apply {
+        color = resources.getColor(android.R.color.holo_red_light, context.theme)
+        style = Paint.Style.FILL
+    }
+
+    private var hour: Int? = null
+    private var minute: Int? = null
+    private var sweepAngle: Float = 0f
+
     fun setTime(hour: Int, minute: Int) {
-        // TODO
+        this.hour = hour
+        this.minute = minute
+        sweepAngle = computeAngle(hour, minute)
+        invalidate()
+    }
+
+    private fun computeAngle(hour: Int, minute: Int): Float {
+        val minuteLimited = minute % 60 // Minute could be > 60
+        val minuteFactor = minuteLimited / 60f // Between 0 and 1
+        return 360 * minuteFactor // Between 0 and 360
+
+        // Ignore hour for now
     }
 
     override fun onDraw(canvas: Canvas) {
 
-        // Draw background
+        // Clock frame parameters
         val halfWidth = measuredWidth / 2f
         val halfHeight = measuredHeight / 2f
         val radius = min(halfWidth, halfHeight) - halfStrokeWidth
-        canvas.drawCircle(halfWidth, halfHeight, radius, paint)
 
-        // TODO: Draw time
+        // Clock hands parameters
+        val left = halfWidth - radius
+        val top = halfHeight - radius
+        val right = halfWidth + radius
+        val bottom = halfHeight + radius
+
+        canvas.drawArc(
+            left,
+            top,
+            right,
+            bottom,
+            -90f,
+            sweepAngle,
+            true,
+            arcPaint
+        )
+
+        canvas.drawCircle(halfWidth, halfHeight, radius, circlePaint)
     }
 }
